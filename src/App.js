fbc40b52ac75cf66b4df55e9e939ad43ffe32d48
G1tgud-00 <93236591+G1tgud-00@users.noreply.github.com>
2023-11-23 00:42:37 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,43 +1,63 @@
-import React, { useEffect, useState } from 'react';
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import logo from "./logo.svg";
 import "./App.css";
 import { LeftMenu } from "./Components/LeftMenu";
 import { MainContainer } from "./Components/MainContainer";
-import { Register } from './Components/Register';
 import { RightMenu } from "./Components/RightMenu";
-function Navigation() {
-  return (
-    <div>
-    <ul>
-      <li><Link to="">Home</Link></li>
-      <li><a href="/Bay">Bay</a></li>
-    </ul>
-    </div>
-  );
-}
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Home from './home';
+import Login from './login';
+import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function App() {
-  const navigate = useNavigate();
-  const [showText, setShowText] = useState(true);
+
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [email, setEmail] = useState("")
 
   useEffect(() => {
-    // Check the current route and update state accordingly
-    setShowText(window.location.pathname === '/');
-  }, []);
+    // Fetch the user email and token from local storage
+    const user = JSON.parse(localStorage.getItem("user"))
+
+    // If the token/email does not exist, mark the user as logged out
+    if (!user || !user.token) {
+      setLoggedIn(false)
+      return
+    }
+
+    // If the token exists, verify it with the auth server to see if it is valid
+    fetch("http://localhost:3080/verify", {
+            method: "POST",
+            headers: {
+                'jwt-token': user.token
+              }
+        })
+        .then(r => r.json())
+        .then(r => {
+            setLoggedIn('success' === r.message)
+            setEmail(user.email || "")
+        })
+  }, [])
+
+  function Navigation() {
+    return (
+      <div>
+      <ul>
+        <li><Link to="">Home</Link></li>
+        <li><a href="/Bay">Bay</a></li>
+      </ul>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
-      {showText && <Navigation />}
 
-      <div className="background"></div>
-
-      <Routes>
-        <Route path="" element={
-          <>
-            <Register/>
-          </>
-        } />
-        <Route path="/Bay" element={
+<BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home email={email} loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>} /> 
+          <Route path="/login" element={<Login setLoggedIn={setLoggedIn} setEmail={setEmail} />} />
+          <Route path="/Bay" element={
           <>
             <LeftMenu />
             <MainContainer />
@@ -45,9 +65,14 @@ function App() {
             
           </>
         } />
-      </Routes>
+        </Routes>
+       
+      </BrowserRouter>
+      
+      <div className="background"></div>
     </div>
   );
+  
 }
 
 export default App;
