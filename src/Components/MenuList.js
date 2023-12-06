02442180c641194ce0366e5d079f0d38069fdc7d
GBao294 <105119912+GBao294@users.noreply.github.com>
2023-12-06 23:55:17 +0700

    Update MenuList.js

--- src/Components/MenuList.js
+++ src/Components/MenuList.js
@@ -24,7 +24,7 @@ const MenuList = [
     id: 4,
     icon: <BsPersonExclamation />,
     name: "Donate",
-    link: "https://test-payment.momo.vn/v2/gateway/pay?t=TU9NT3xNT01PMTcwMTg4MTA4NjkyNQ&s=d2e3f9d570ec617786157a3f96fc6aa99b8ccc3dbab2abee40635a99f480df5c"
+    link: "https://test-payment.momo.vn/v2/gateway/pay?t=TU9NT3xNT01PMTcwMTg4MTY1NzY0NA&s=07d9dbe183d5ea2b9b5306fd41b4210e16e303e5eb7e38e291bbf25078d06fd8"
   },
   // {
   //   id: 5,
